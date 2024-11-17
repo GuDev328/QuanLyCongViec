1fec33ec81e7a39ea660b71fb716b0805b53b3cd
GuDev <datminiphi@gmail.com>
2024-11-17 22:05:14 +0700

    Update Task

--- app/src/main/java/com/example/quanlycongviec/TaskAction/TaskActionView.java
+++ app/src/main/java/com/example/quanlycongviec/TaskAction/TaskActionView.java
@@ -1,5 +1,6 @@
 package com.example.quanlycongviec.TaskAction;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -28,6 +33,23 @@ public class TaskActionView extends AppCompatActivity {
     TaskDAO taskDAO;
     CategoryDAO categoryDAO;
     ImageView iconEdit, iconDelete;
+
+    //Chạy lại khi đóng activity khác
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        //Đánh dấu để trả về Activity trước để chạy hàm định sẵn
+                        Intent resultIntent = new Intent();
+                        setResult(RESULT_OK, resultIntent);  // Gửi kết quả về Activity 1
+                        finish();
+
+                    }
+                }
+            });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -95,6 +117,17 @@ public class TaskActionView extends AppCompatActivity {
                 }
             });
             
+
+            iconEdit =findViewById(R.id.iconEdit);
+            iconEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(TaskActionView.this, TaskActionEdit.class);
+                    intent.putExtra("selectedIdTask",task.getId());
+                    activityResultLauncher.launch(intent);
+                }
+            });
+
 
         } else {
             Toast.makeText(this, "Không xác định được công việc!", Toast.LENGTH_SHORT).show();
